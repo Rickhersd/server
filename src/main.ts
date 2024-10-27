1ce493167abd41670c5f0c0715c50c908689182c
Rickhersd <rickhersd2002@gmail.com>
2024-10-27 19:12:30 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,7 @@ async function bootstrap() {
          'htps://course-platform-gamma-green.vercel.app'
     ];
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-      res.setHeader('Access-Control-Allow-Origin', req.headers.origin );
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin );;
     }
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
